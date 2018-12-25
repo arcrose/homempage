f4659480861136cdc35b354b1e0789a312913d73
Emma Rose <emma.arcadia.rose@gmail.com>
2018-12-25 18:46:34 -0500

    Foundation for code analysis state machine based on pure functions and mutual recursion

--- src/code.rs
+++ src/code.rs
@@ -27,6 +27,34 @@ pub struct SourceCodeDirectory {
   pub source_files: Vec<Source>,
 }
 
-pub fn analyze<P: AsRef<Path>>(directory: P) -> Result<Vec<SourceCodeDirectory>, io::Error> {
+pub Analysis(AnalysisState);
+
+pub type AnalysisResult = Result<Vec<SourceCodeDirectory>, io::Error>;
+
+enum AnalysisState {
+  Start(Path),
+  KnowWhereToSearch(Vec<Path>),
+  Finish(Vec<SourceCodeDirectory>),
+}
+
+pub fn analyze<P: AsRef<Path>>(directory: P) -> Analysis {
+  Analysis(AnalysisState::Start(p.as_ref().clone()))
+}
+
+pub fn run(a: Analysis(state)) -> AnalysisResult {
+  use AnalysisState::*;
+
+  match state {
+    Start(path)              => search_for_snippets(path)
+    KnowWhereToSearch(paths) => analyze_sources(paths),
+    Finish(results)          => Ok(results),
+  }
+}
+
+fn search_for_snippets(base: Path) -> AnalysisResult {
+  Ok(vec![])
+}
+
+fn analyze_sources(file_paths: Vec<Path>) -> AnalysisResult {
   Ok(vec![])
 }
