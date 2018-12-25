7821dfdf30543aa3e068c73969a1d1183cef6534
Emma Rose <emma.arcadia.rose@gmail.com>
2018-12-25 15:44:54 -0500

    Much more satisfying character limit computation

--- js/main.js
+++ js/main.js
@@ -2,7 +2,6 @@
   const codeSegmentTop = [3, 5]            // Start drawing code from x=3, y=5
   const codeSegmentIdentLineDelta = [6, 3] // Add x+5, y+3 to indent the next line of code
 
-
   const text = (x, y, className, text) => {
     let newNode = document.createElementNS('http://www.w3.org/2000/svg', 'text');
     newNode.setAttribute('x', `${x}`)
@@ -18,7 +17,7 @@
     const startX = indent * codeSegmentIdentLineDelta[0] + codeSegmentTop[0]
     const startY = line * codeSegmentIdentLineDelta[1] + codeSegmentTop[1]
 
-    const charLimit = 64 * (1284 / window.innerWidth) - 2.25 * line - 5 * indent
+    const charLimit = 80 - (3.15 * line) - (4 * indent)
 
     return text(startX, startY, 'code', code.slice(0, charLimit))
   }
@@ -26,12 +25,11 @@
   const codeSegment = document.getElementById('diagonal-code-segment')
   const writingSegment = document.getElementById('diagonal-writing-segment')
 
-  codeSegment.appendChild(code(0, 0, 'def factorial(n, acc=1):'))
-  codeSegment.appendChild(code(1, 1, 'if n <= 1:'))
-  codeSegment.appendChild(code(2, 2, 'return acc'))
-  codeSegment.appendChild(code(3, 1, 'print("This is a really stupid long line of text frankly you should just pretend"'))
-  codeSegment.appendChild(code(4, 1, 'print("This is a really stupid long line of text frankly you should just pretend"'))
-  codeSegment.appendChild(code(5, 2, 'print("This is a really stupid long line of text frankly you should just pretend"'))
+  codeSegment.appendChild(code(0, 0, 'print("This is a really stupid long line of text frankly you should just pretend"'))
+  codeSegment.appendChild(code(1, 0, 'print("This is a really stupid long line of text frankly you should just pretend"'))
+  codeSegment.appendChild(code(2, 1, 'print("This is a really stupid long line of text frankly you should just pretend"'))
+  codeSegment.appendChild(code(3, 2, 'print("This is a really stupid long line of text frankly you should just pretend"'))
+  codeSegment.appendChild(code(4, 0, 'print("This is a really stupid long line of text frankly you should just pretend"'))
 
   writingSegment.appendChild(text(10, 95, 'word', 'Test'))
 })()
