0636249fc6e787619bb226aa4d5f3726e3c82d04
Emma Rose <emma.arcadia.rose@gmail.com>
2018-12-24 19:39:33 -0500

    Adding foundations of functionality for drawing syntax-highlighted source code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,22 @@
 (function() {
-  console.log('Hello, world!')
+  const drawText = (container, x, y, classes, text) => {
+    let newNode = document.createElementNS('http://www.w3.org/2000/svg', 'text');
+    newNode.setAttribute('x', `${x}`)
+    newNode.setAttribute('y', `${y}`)
+    newNode.textContent = text
+
+    for (const className of classes) {
+      newNode.classList.add(className)
+    }
+
+    container.appendChild(newNode)
+
+    return newNode
+  }
+
+  const codeSegment = document.getElementById('diagonal-code-segment')
+  const writingSegment = document.getElementById('diagonal-writing-segment')
+
+  drawText(codeSegment, 3, 5, ['code', 'red'], 'def test():')
+  drawText(codeSegment, 9, 8, ['code', 'blue'], 'print("Hello, world")')
 })()
